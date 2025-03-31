0762c4dd91837995500df261d79c4aca1c433cd6
sidath samarasinghe <sidaath@gmail.com>
2025-03-31 23:33:19 +0200

    read entire file with multiple classes without adding class names to list

--- TokenizerTester.java
+++ TokenizerTester.java
@@ -1,4 +1,5 @@
 import java.util.Deque;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.ArrayDeque;
 import java.io.*;
@@ -6,27 +7,30 @@ import java.lang.Integer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-class Main
-{
+class TokenizerTester {
 	public static void main (String[] args){
 		//extend later to get a full queue of directories to search
 		try{
-			String fileContent = Files.readString(Path.of(args[0]));
-			System.out.println(fileContent);
-			System.out.println("**** end of file *******\n");
+			System.out.println("SOurce File : "+args[0]);
+			System.out.println("Result File : "+args[1]);
 			File file = new File(args[0]);
-			ArrayList<String> res = tokenize(file);
-			System.out.println("done tokenizing");
-			int index = 0;
-			for(String s : res){
-				System.out.println(index+" = "+s);
-				index++;
+			List<String> res = tokenize(file);
+			InputStream inStream = new FileInputStream(file);
+			Reader readerRaw = new InputStreamReader(inStream);
+			BufferedReader reader = new BufferedReader(readerRaw);
+			List<String> compare = Files.readAllLines(Path.of(args[1]));
+			for(int i = 0; i < res.size(); i++){
+				String a = res.get(i);
+				String b = compare.get(i);
+				if(!(a.equals(b))){
+					System.out.println(a+" != "+b+" at index "+i);
+				}
 			}
+			System.out.println("Pass");
 		}
 		catch(Exception e){
 			System.out.println(e);
 		}
-		System.out.println("done");
 	}
 
 	private static ArrayList<String> tokenize(File file){
